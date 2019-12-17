daab5b45602bcdca58d900511d6a2af2fcabfe00
William <wilindho@gmail.com>
2019-12-17 17:25:37 +0000

    Djur classen

--- spring/src/spring/djur.java
+++ spring/src/spring/djur.java
@@ -2,19 +2,10 @@ package spring;
 
 public class djur {
 
-	String[] djurtyp = {"Lion", "Panther", "Hippopottamus", "Cat", "Dog"};
+	static String djurtyp;
+	static int maxSpeed;
+	static int minSpeed;
+	static float eat;
 
-	int[] maxSpeed = { 45, 53, 37, 39, 41};	//tas * 0,1 för att bli rätt
-	int[] minSpeed = {35, 37, 33, 33, 35};
-
-	float[] eat = { 0.13 ,0.23 ,0.02, 0.03 , 0.09};
-
-	float rundaSpeed{
-		for(i = 0; i = length.djurtyp ; i++) {
-			Random rand = new Random();
-
-			int value = rand.nextInt((maxSpeed(i) - minSpeed(i) + 1 ) + minSpeed;
-
-		}
-	}
+	
 }
