3dc35808d6b178334c357acc52bab2230a1c77dc
Shaliela <kristina@causton.net>
2019-12-17 19:08:30 +0200

    test 2

--- spring/race/djur.java
+++ spring/race/djur.java
@@ -1,5 +1,19 @@
 package race;
 
 public class djur {
-
+	String djurtyp {"Lion", "Panther", "Hippopottamus", "Cat", "Dog"};
+	
+	int maxSpeed { 45 ; 53 ; 37 ; 39 ; 41 };	//tas * 0,1 för att bli rätt
+	int minSpeed {35 ; 37 ; 33 ; 33 ; 35};
+	
+	float eat { 0.13 ; 0.23 ; 0,02 ; 0,03 ; 0,09 };
+	
+	float rundaSpeed{
+		for(i = 0; i = length.djurtyp ; i++) {
+			Random rand =new Random();
+		
+			int value = rand.nextInt((maxSpeed(i) - minSpeed(i) + 1 ) + minSpeed;
+		
+		}
+	}
 }
