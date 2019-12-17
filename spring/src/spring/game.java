bd88d5923f67876f6fa81bf5f94da63d6da7055b
Shaliela <kristina@causton.net>
2019-12-17 19:46:52 +0200

    arrays for djuren

--- spring/src/spring/game.java
+++ spring/src/spring/game.java
@@ -5,12 +5,12 @@ public class game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String djurtyp {"Lion", "Panther", "Hippopottamus", "Cat", "Dog"};
+		String[] names = {"Lion", "Panther", "Hippopottamus", "Cat", "Dog"};
 
-		int maxSpeed { 45 ; 53 ; 37 ; 39 ; 41 };	//tas * 0,1 för att bli rätt
-		int minSpeed {35 ; 37 ; 33 ; 33 ; 35};
+		int[] maxspeed = {45, 53, 37, 39, 41};	//tas * 0,1 for att bli ratt
+		int[] minspeed = {35, 37, 33, 33, 35};
 
-		float eat { 0.13 ; 0.23 ; 0,02 ; 0,03 ; 0,09 };
+		double[] eatchance = { 0.13 , 0.23 , 0.02 , 0.03 , 0.09 };
 		
 		djur[] djuren = new djur[4];
 		for (int i = 0; i < djuren.length; i++) {
