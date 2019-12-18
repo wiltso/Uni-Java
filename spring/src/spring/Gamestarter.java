40fcc1953a127851678a0685efa81b7cd01fc358
William <wilindho@gmail.com>
2019-12-18 09:11:42 +0000

    Ready for comments

--- spring/src/spring/Gamestarter.java
+++ spring/src/spring/Gamestarter.java
@@ -1,5 +1,9 @@
 package spring;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Gamestarter {
 
 	public static void main(String[] args) {
@@ -7,6 +11,81 @@ public class Gamestarter {
 		game thegame = new game();
 		thegame.runGame();
 		
+		/*
+		 * For testing
+		thegame.initilize();
+		for(int i = 0; i<5000000;i++) {
+			thegame.runGame();
+		} 
+		int hippo = 0;
+		int lion = 0;
+		int bunny = 0;
+		int panther = 0;
+		int cat = 0;
+		int dog = 0;
+		
+		int win1 = 0;
+		int win2 = 0;
+		int win3 = 0;
+		int win4 = 0;
+		int win5 = 0;
+		int win6 = 0;
+		
+		
+		for(int i = 0; i<5000000;i++) {
+			if (thegame.unsortedPlayers.get(i).contains("The Hippo")){
+				hippo = hippo + 1;
+			}
+			if (thegame.unsortedPlayers.get(i).contains("The Lion")){
+				lion = lion + 1;
+			}
+			if (thegame.unsortedPlayers.get(i).contains("The Cat")){
+				cat = cat + 1;
+			}
+			if (thegame.unsortedPlayers.get(i).contains("The Dog")){
+				dog = dog + 1;
+			}
+			if (thegame.unsortedPlayers.get(i).contains("The Panther")){
+				panther = panther + 1;
+			}
+			if (thegame.unsortedPlayers.get(i).contains("The Bunny")){
+				bunny = bunny + 1;
+			}
+			List<String> list = Arrays.asList(thegame.unsortedPlayers.get(i).split(" "));
+			if (Collections.frequency(list, "and") == 1) {
+				win1 = win1 + 1;
+			}
+			if (Collections.frequency(list, "and") == 2) {
+				win2 = win2 + 1;
+			}
+			if (Collections.frequency(list, "and") == 3) {
+				win3 = win3 + 1;
+			}
+			if (Collections.frequency(list, "and") == 4) {
+				win4 = win4 + 1;
+			}
+			if (Collections.frequency(list, "and") == 5) {
+				win5 = win5 + 1;
+			}
+			if (Collections.frequency(list, "and") == 6) {
+				win6 = win6 + 1;
+			}
+			//System.out.println(thegame.unsortedPlayers.get(i));
+		}
+		System.out.println("1 winner: " + Integer.toString(win1));
+		System.out.println("2 winner: " + Integer.toString(win2));
+		System.out.println("3 winner: " + Integer.toString(win3));
+		System.out.println("4 winner: " + Integer.toString(win4));
+		System.out.println("5 winner: " + Integer.toString(win5));
+		System.out.println("6 winner: " + Integer.toString(win6));
+		
+		System.out.println("dog won: " + Integer.toString(dog));
+		System.out.println("cat won: " + Integer.toString(cat));
+		System.out.println("hippo won: " + Integer.toString(hippo));
+		System.out.println("lion won: " + Integer.toString(lion));
+		System.out.println("panther won: " + Integer.toString(panther));
+		System.out.println("bunny won: " + Integer.toString(bunny));
+		*/
 	}
 
 }
--- spring/src/spring/game.java
+++ spring/src/spring/game.java
@@ -1,13 +1,15 @@
 package spring;
 
+import java.util.*;
+
 public class game {
-	
+	HashMap<Integer, String> unsortedPlayers = new HashMap<Integer, String>();
+	int round = 0;
 	djur[] djuren = new djur[6];
   
-	String format = "%1$-40s";
+	String format = "%1$-20s";
 	
 	public void runGame() {
-		// TODO Auto-generated method stub
 		initilize();
 		boolean won = false;
 		while (won == false) {
@@ -15,24 +17,30 @@ public class game {
 			won = run();
 		}
 		spelslut();
+		/* For testing
 		reset();
+		 */
 	}
 	
-	private void initilize() {
-		String[] names = {"Lion", "Panther", "Hippo", "Cat", "Dog", "Bunny"};
-		double[] maxspeed = {4.5, 5.3, 3.7, 3.9, 4.1, 6.6};	//tas * 0,1 for att bli ratt
-		double[] minspeed = {3.5, 3.7, 3.3, 3.3, 3.5, 0.2};
-		double[] eatchance = { 0.13 , 0.23 , 0.02 , 0.03 , 0.09, 0.02 };
+	public void reset() {
+		for (int i = 0; i < djuren.length; i++) {
+			djuren[i].reset();
+		}
+	}
+	
+	public void initilize() {
+		String[] names = 	{"Lion", 	"Panther", 	"Hippo", 	"Cat", 	"Dog", 	"Bunny"};
+		double[] maxspeed = {4.5, 		5.3, 		3.7, 		3.9, 	4.1, 	6.6};
+		double[] minspeed = {3.5, 		3.7, 		3.3, 		3.3, 	3.5, 	0.2};
+		double[] eatchance = { 0.13 , 	0.23 , 		0.02, 		0.03 , 	0.09, 	0.02 };
 		
 		for (int i = 0; i < djuren.length; i++) {
 			djuren[i] = new djur();
 			djuren[i].initilize(names[i], maxspeed[i], minspeed[i], eatchance[i]);
 		}
 		
-		for (djur djuret:djuren) {	//fr alla djuren
-			System.out.format(format, djuret.getTheName());
-			djuret.getTheName();	//hmtar djurets namn med The
-			
+		for (djur djuret:djuren) {
+			System.out.format(format, djuret.getTheName());			
 		}
 	}
 	
@@ -43,7 +51,7 @@ public class game {
 			if (runningTotal == -1.0) {
 				System.out.format(format, "Eats");
 			}else {
-				System.out.format(format, runningTotal);
+				System.out.format(format, String.format ("%.2f", runningTotal));
 			}
 			if (someOneWon != true){
 				someOneWon = djuret.won();
@@ -52,11 +60,6 @@ public class game {
 		return someOneWon;
 	}
 	
-	private void reset() {
-		for (djur djuret:djuren) {
-			djuret.reset();
-		}
-	}
 	public void spelslut() {
 		String vinnare = "";
 		for (djur djuret:djuren) {
@@ -64,6 +67,8 @@ public class game {
 				vinnare = vinnare + djuret.getTheName() + " and ";
 			}
 		}
+		unsortedPlayers.put(round, vinnare);
+		round = round + 1;
 		System.out.println();
 		System.out.println("The competition was won by " + vinnare.substring(0,vinnare.length()-5));
 	}
